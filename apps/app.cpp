bba19c94492933551ca98565809514efe031906a
Kerry Wang <kerrywang369@gmail.com>
2023-07-31 11:42:55 -0600

    progress

--- apps/app.cpp
+++ apps/app.cpp
@@ -6,12 +6,21 @@
 #include <vector>
 #include <tuple>
 
-int main() {
+int main()
+{
     std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
 
     auto [mean, moment] = accumulate_vector(input);
 
-    fmt::print("Mean: {}, Moment: {}\n",  mean, moment);
+    fmt::print("Mean: {}, Moment: {}\n", mean, moment);
+
+    auto keyboard = KeyboardHandler();
+    keyboard.setup();
+
+    while (1)
+    {
+        std::cout << toString(keyboard.loop()) << std::endl;
+    }
 
     return 0;
 }
--- src/lib.cpp
+++ src/lib.cpp
@@ -3,6 +3,7 @@
 #include <tuple>
 #include <vector>
 #include <algorithm>
+#include <ncurses.h>
 
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/stats.hpp>
@@ -11,11 +12,53 @@
 
 namespace ba = boost::accumulators;
 
-std::tuple<double, double> accumulate_vector(const std::vector<double>& values) {
+std::tuple<double, double> accumulate_vector(const std::vector<double> &values)
+{
 
     ba::accumulator_set<double, ba::stats<ba::tag::mean, ba::tag::moment<2>>> acc;
 
     std::for_each(std::begin(values), std::end(values), std::ref(acc));
 
     return {ba::mean(acc), ba::moment<2>(acc)};
+}
+
+void KeyboardHandler::end()
+{
+    // End ncurses
+    endwin();
+}
+
+void KeyboardHandler::setup()
+{
+    // Initialize ncurses
+    initscr();
+    cbreak();             // Line buffering disabled
+    noecho();             // Don't echo input characters
+    keypad(stdscr, true); // Enable the keypad for arrow keys
+
+    // Print instructions
+    printw("Press the arrow keys. Press 'q' to quit.\n");
+}
+
+Action KeyboardHandler::loop()
+{
+    if (int ch = getch(); ch != 'q')
+    {
+        // Check for arrow keys
+        switch (ch)
+        {
+        case KEY_UP:
+            return Action::UP;
+        case KEY_DOWN:
+            return Action::DONE;
+        case KEY_LEFT:
+            return Action::LEFT;
+        case KEY_RIGHT:
+            return Action::RIGHT;
+        default:
+            printw("Press the arrow keys. Press 'q' to quit.\n");
+            break;
+        }
+    }
+    return Action::DONE;
 }
